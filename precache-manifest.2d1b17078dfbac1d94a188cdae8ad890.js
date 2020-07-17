8ae23d3e42c0381eeeff1f4c652e91d4bed9431f
sgstreeks <sgshankar251996@gmail.com>
2020-07-17 19:33:51 +0530

    Updates

---
+++ precache-manifest.2d1b17078dfbac1d94a188cdae8ad890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8629ff2877ac7f794f53aa1fefb188",
+    "url": "/minipekka25/waatsapp_clone/index.html"
+  },
+  {
+    "revision": "78b42d5a79d3ce29140f",
+    "url": "/minipekka25/waatsapp_clone/static/css/main.8d78ae90.chunk.css"
+  },
+  {
+    "revision": "c7a9a93a88b126b0698f",
+    "url": "/minipekka25/waatsapp_clone/static/js/2.10b15772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minipekka25/waatsapp_clone/static/js/2.10b15772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b42d5a79d3ce29140f",
+    "url": "/minipekka25/waatsapp_clone/static/js/main.b5beb9a2.chunk.js"
+  },
+  {
+    "revision": "5b148c22989faca791cf",
+    "url": "/minipekka25/waatsapp_clone/static/js/runtime-main.3acf7df8.js"
+  }
+]);
